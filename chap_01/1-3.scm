1a57db866e116413afe1aa65561375a7d8030ef9
Duglas Yun <yun.douglas@gmail.com>
2013-05-09 23:01:56 -0400

    first!

---
+++ chap_01/1-3.scm
@@ -0,0 +1,19 @@
+(define (sum-of-sqaure-for-two-largest x y z)
+    if (= x (which-is-larger x y)
+          (sum-of-sqaures x (which-is-larger y z))
+          (sum-of-sqaures y (which-is-larger x z))
+          )
+  )
+
+(define (sum-of-sqaures a b)
+    (+ (sqaure a) (sqaure b))
+  )
+
+(define (sqaure number) (* number number))
+
+(define (which-is-larger a b)
+    (if (> a b)
+      a
+      b
+      )
+  )
