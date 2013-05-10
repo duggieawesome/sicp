4ddca1b68d0de83692b2cd068d9a73ea3e2c6fec
Duglas Yun <yun.douglas@gmail.com>
2013-05-10 00:03:54 -0400

    corrected the missing parens

--- chap_01/1-3.scm
+++ chap_01/1-3.scm
@@ -1,13 +1,13 @@
 (define (sum-of-sqaure-for-two-largest x y z)
-    if (= x (which-is-larger x y)
-          (sum-of-sqaures x (which-is-larger y z))
-          (sum-of-sqaures y (which-is-larger x z))
-          )
-  )
+     (if (= x (which-is-larger x y))
+         (sum-of-sqaures x (which-is-larger y z))
+         (sum-of-sqaures y (which-is-larger x z))
+     )
+)
 
 (define (sum-of-sqaures a b)
     (+ (sqaure a) (sqaure b))
-  )
+)
 
 (define (sqaure number) (* number number))
 
@@ -15,5 +15,5 @@
     (if (> a b)
       a
       b
-      )
-  )
+    )
+)
