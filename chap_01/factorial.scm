5c2adc3210b5d5e512fb89ff89ae9066d05e5284
Duglas Yun <yun.douglas@gmail.com>
2013-05-28 23:01:23 -0400

    factorial - recursive and iterative

---
+++ chap_01/factorial.scm
@@ -0,0 +1,18 @@
+; factorial - linear recursive
+
+(define (factorial n)
+  (if (= n 1)
+    1
+    (* n (factorial (- n 1)))))
+
+; factorial - linear iterative
+
+(define (factorial-iter n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+    product
+    (fact-iter (* counter product)
+               (+ counter 1)
+               max-count)))
