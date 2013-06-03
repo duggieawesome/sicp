6750528a7608a525bac377c356798777fd26a1b7
Doug Yun <yun.douglas@gmail.com>
2013-06-03 00:22:03 -0400

    quick rename of dir

---
+++ chap-01/prime.scm
@@ -0,0 +1,18 @@
+; this is kinda janky right now.
+
+(define (prime? number)
+  (= number (smallest-divisor number)))
+
+(define (smallest-divisor number)
+  (find-divisor number 2))
+
+(define (find-divisor number test-divisor)
+  (cond ((> (square test-divisor) number) number)
+        ((divides? test-divisor number) test-divisor)
+        (else (find-divisor number (+ test-divisor 1)))))
+
+(define (square x)
+  (* x x))
+
+(define (divides? a b)
+  (= remainder b a) 0)
